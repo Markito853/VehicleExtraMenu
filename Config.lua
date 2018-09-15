7812a9d546dd45eabfe54d7edefaa48e1e45829a
Flatracer <flatracer@ymail.com>
2018-09-15 15:27:28 +0200

    Update to version 2.0.0
    - Using NativeUILua now

---
+++ Config.lua
@@ -0,0 +1,6 @@
+--Change this to edit the Opening Key (Other Keys at wiki.fivem.net/wiki/Controls)
+VEM.KBKey = 56
+
+--Change "false" to "true" to disable the Menu for Non-Admins
+VEM.OnlyForAdmins = false
+
--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,21 @@
-description "Vehicle Extra Menu" -- Resource Descrption
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description "Vehicle Extra Menu" 	-- Resource Descrption
+
+dependencies {
+    'NativeUI',
+}
 
 client_script {
-	'client/client.lua',
-	'client/GUI.lua',
+	'@NativeUI/NativeUI.lua',
+	'Client/Preload.lua',
+	'Config.lua',
+	'Client/Client.lua',
+}
+
+
+server_script {
+	'Server/Server.lua',
 }
 
 -- Vehicle Extra Menu Made By: FlatracerMOD (aka Flatracer)
